18a7389a969434a4b625beea767a4c636475588c
Kautilya Reddy <kautilya97@gmail.com>
2023-10-06 00:16:22 +0000

    fixed test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -7,8 +7,14 @@ TEST(PIDControllerTest, Compute) {
     // Test case 1: Target and actual velocity are equal
     double result1 = pid.compute(10.0, 10.0);
     EXPECT_NEAR(result1, 0.0, 0.1);
+}
+
+
+TEST(PIDControllerTest, Compute) {
+    PIDController pid(1.0, 0.1, 0.01);
 
     // Test case 2: Target velocity is higher than actual
     double result2 = pid.compute(20.0, 15.0);
     EXPECT_NEAR(result2, 5.6, 0.1);
 }
+
