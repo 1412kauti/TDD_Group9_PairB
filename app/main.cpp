31584255093c38b1f90296ebc9e26c7df2d490e4
Vikram Setty <vikramsetty169@gmail.com>
2023-10-06 23:50:50 -0400

    Added functionality to the code in the app/main.cpp file and the libs implementation file. All testcases now pass. Good to go

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,23 @@
 #include "PIDController.hpp"
+#include <iostream>
 
 int main() {
-  return 0;
+    // Initialize the PID controller with tuning parameters (Kp, Ki, Kd)
+    PIDController pid(1.0, 0.1, 0.01);
+
+    // Simulate a control loop
+    double targetVelocity = 20.0;  // The desired target velocity
+    double actualVelocity = 0.0;  // The current actual velocity
+
+    for (int i = 0; i < 100; ++i) {
+        // Calculate the control output
+        double controlOutput = pid.compute(targetVelocity, actualVelocity);
+
+        // Simulate the system (e.g., adjust actual velocity based on the control output)
+        actualVelocity += controlOutput * 0.01;  // Assuming a time step of 0.01 seconds
+
+        std::cout << "Time: " << i * 0.01 << " seconds, Actual Velocity: " << actualVelocity << std::endl;
+    }
+
+    return 0;
 }
